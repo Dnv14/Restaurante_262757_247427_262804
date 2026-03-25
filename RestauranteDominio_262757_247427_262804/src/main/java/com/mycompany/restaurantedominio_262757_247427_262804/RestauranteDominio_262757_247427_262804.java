26d98b8a472ba735850ad4f6d9d1becc4e0ab6c1
Dnv14 <diego.navarro.140706@gmail.com>
2026-03-24 19:33:00 -0700

    dependencias
    dependencias

---
+++ RestauranteDominio_262757_247427_262804/src/main/java/com/mycompany/restaurantedominio_262757_247427_262804/RestauranteDominio_262757_247427_262804.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.restaurantedominio_262757_247427_262804;
+
+/**
+ *
+ * @author Diego
+ */
+public class RestauranteDominio_262757_247427_262804 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
---
+++ RestauranteDtos_262757_247427_262804/src/main/java/com/mycompany/restaurantedtos_262757_247427_262804/RestauranteDtos_262757_247427_262804.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.restaurantedtos_262757_247427_262804;
+
+/**
+ *
+ * @author Diego
+ */
+public class RestauranteDtos_262757_247427_262804 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
