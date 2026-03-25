715ac57550f92a1f299e821056a392c7136b3e55
Dnv14 <diego.navarro.140706@gmail.com>
2026-03-24 23:44:28 -0700

    JPA
    agregacion JPA

---
+++ RestauranteDominio_262757_247427_262804/src/main/java/com/mycompany/restaurantedominio_262757_247427_262804/Cliente.java
@@ -0,0 +1,53 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.restaurantedominio_262757_247427_262804;
+
+import java.io.Serializable;
+
+/**
+ *
+ * @author Diego
+ */
+
+public class Cliente implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+   
+    private Long id;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (id != null ? id.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof Cliente)) {
+            return false;
+        }
+        Cliente other = (Cliente) object;
+        if ((this.id == null && other.id != null) || (this.id != null && !this.id.equals(other.id))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "com.mycompany.restaurantedominio_262757_247427_262804.Cliente[ id=" + id + " ]";
+    }
+    
+}
