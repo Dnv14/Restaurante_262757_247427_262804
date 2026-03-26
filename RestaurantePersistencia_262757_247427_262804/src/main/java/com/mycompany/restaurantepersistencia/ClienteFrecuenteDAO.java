f5ae628590a169768a2aa072236fbeeec2b6be30
Dnv14 <diego.navarro.140706@gmail.com>
2026-03-25 22:32:57 -0700

    jpql
    dao obtener todos

--- RestaurantePersistencia_262757_247427_262804/src/main/java/com/mycompany/restaurantepersistencia/ClienteFrecuenteDAO.java
+++ RestaurantePersistencia_262757_247427_262804/src/main/java/com/mycompany/restaurantepersistencia/ClienteFrecuenteDAO.java
@@ -7,10 +7,12 @@ package com.mycompany.restaurantepersistencia;
 import com.mycompany.restaurantedominio_262757_247427_262804.ClienteFrecuente;
 import com.mycompany.restaurantedtos_262757_247427_262804.NuevoClienteDTO;
 import java.time.LocalDate;
+import java.util.List;
 import javax.persistence.criteria.Predicate;
 import java.util.logging.Logger;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceException;
+import javax.persistence.TypedQuery;
 import javax.persistence.criteria.CriteriaBuilder;
 import javax.persistence.criteria.CriteriaQuery;
 import javax.persistence.criteria.Root;
@@ -19,10 +21,9 @@ import javax.persistence.criteria.Root;
  *
  * @author BALAMRUSH
  */
-public class ClienteFrecuenteDAO implements IClienteFrecuenteDAO{
+public class ClienteFrecuenteDAO implements IClienteFrecuenteDAO {
 
     private static final Logger LOGGER = Logger.getLogger(ClienteFrecuenteDAO.class.getName());
-    
 
     @Override
     public ClienteFrecuente crearClienteFrecuente(NuevoClienteDTO nuevoClienteFrecuente) throws PersistenciaException {
@@ -31,21 +32,21 @@ public class ClienteFrecuenteDAO implements IClienteFrecuenteDAO{
         clienteFrecuente.setCorreoElectronico(nuevoClienteFrecuente.getCorreoElectronico());
         clienteFrecuente.setTelefono(nuevoClienteFrecuente.getTelefono());
         clienteFrecuente.setFechaRegistro(LocalDate.now());
-        try{
+        try {
             EntityManager entityManager = ManejadorConexiones.crearEntityManager();
             entityManager.getTransaction().begin();
             entityManager.persist(clienteFrecuente);
             entityManager.getTransaction().commit();
             return clienteFrecuente;
-        }catch(PersistenceException ex){
+        } catch (PersistenceException ex) {
             LOGGER.severe(ex.getMessage());
             throw new PersistenciaException("No fue posbile crear el cliente.");
-        }    
+        }
     }
-    
+
     @Override
     public ClienteFrecuente consultarClienteFrecuentePorNombre(String nombreCompleto) throws PersistenciaException {
-        try{
+        try {
             EntityManager entityManager = ManejadorConexiones.crearEntityManager();
             CriteriaBuilder cBuilder = entityManager.getCriteriaBuilder();
             CriteriaQuery<ClienteFrecuente> cQuery = cBuilder.createQuery(ClienteFrecuente.class);
@@ -53,7 +54,7 @@ public class ClienteFrecuenteDAO implements IClienteFrecuenteDAO{
             Predicate predicado = cBuilder.equal(root.get("nombreCompleto"), nombreCompleto);
             cQuery.select(root).where(predicado);
             return entityManager.createQuery(cQuery).getSingleResult();
-        }catch(PersistenceException ex){
+        } catch (PersistenceException ex) {
             LOGGER.severe(ex.getMessage());
             throw new PersistenciaException("No se pudo consultar el cliente frecuente por el nombre");
         }
@@ -61,7 +62,7 @@ public class ClienteFrecuenteDAO implements IClienteFrecuenteDAO{
 
     @Override
     public ClienteFrecuente consultarClienteFrecuentePorTelefono(String telefono) throws PersistenciaException {
-        try{
+        try {
             EntityManager entityManager = ManejadorConexiones.crearEntityManager();
             CriteriaBuilder cBuilder = entityManager.getCriteriaBuilder();
             CriteriaQuery<ClienteFrecuente> cQuery = cBuilder.createQuery(ClienteFrecuente.class);
@@ -69,7 +70,7 @@ public class ClienteFrecuenteDAO implements IClienteFrecuenteDAO{
             Predicate predicado = cBuilder.equal(root.get("telefono"), telefono);
             cQuery.select(root).where(predicado);
             return entityManager.createQuery(cQuery).getSingleResult();
-        }catch(PersistenceException ex){
+        } catch (PersistenceException ex) {
             LOGGER.severe(ex.getMessage());
             throw new PersistenciaException("No se pudo consultar el cliente frecuente por el nombre");
         }
@@ -77,7 +78,7 @@ public class ClienteFrecuenteDAO implements IClienteFrecuenteDAO{
 
     @Override
     public ClienteFrecuente consultarClienteFrecuentePorCorreo(String correoElectronico) throws PersistenciaException {
-        try{
+        try {
             EntityManager entityManager = ManejadorConexiones.crearEntityManager();
             CriteriaBuilder cBuilder = entityManager.getCriteriaBuilder();
             CriteriaQuery<ClienteFrecuente> cQuery = cBuilder.createQuery(ClienteFrecuente.class);
@@ -85,12 +86,24 @@ public class ClienteFrecuenteDAO implements IClienteFrecuenteDAO{
             Predicate predicado = cBuilder.equal(root.get("correoElectronico"), correoElectronico);
             cQuery.select(root).where(predicado);
             return entityManager.createQuery(cQuery).getSingleResult();
-        }catch(PersistenceException ex){
+        } catch (PersistenceException ex) {
             LOGGER.severe(ex.getMessage());
             throw new PersistenciaException("No se pudo consultar el cliente frecuente por el nombre");
         }
     }
 
-    
-    
+    @Override
+    public List<ClienteFrecuente> consultarTodosClientesFrecuentes() throws PersistenciaException {
+        try {
+            EntityManager entityManager = ManejadorConexiones.crearEntityManager();
+            String queryJPQL = """
+                               SELECT cf FROM ClienteFrecuente cf
+                               """;
+            TypedQuery<ClienteFrecuente> query = entityManager.createQuery(queryJPQL, ClienteFrecuente.class);
+            return query.getResultList();
+        } catch (PersistenceException ex) {
+            LOGGER.severe(ex.getMessage());
+            throw new PersistenciaException("No se pudieron obtener los clientes frecuentes");
+        }
+    }
 }
--- RestaurantePersistencia_262757_247427_262804/src/main/java/com/mycompany/restaurantepersistencia/IClienteFrecuenteDAO.java
+++ RestaurantePersistencia_262757_247427_262804/src/main/java/com/mycompany/restaurantepersistencia/IClienteFrecuenteDAO.java
@@ -6,14 +6,21 @@ package com.mycompany.restaurantepersistencia;
 
 import com.mycompany.restaurantedominio_262757_247427_262804.ClienteFrecuente;
 import com.mycompany.restaurantedtos_262757_247427_262804.NuevoClienteDTO;
+import java.util.List;
 
 /**
  *
  * @author BALAMRUSH
  */
 public interface IClienteFrecuenteDAO {
+
     public abstract ClienteFrecuente crearClienteFrecuente(NuevoClienteDTO nuevoClienteFrecuente) throws PersistenciaException;
+
     public abstract ClienteFrecuente consultarClienteFrecuentePorNombre(String nombreCompleto) throws PersistenciaException;
+
     public abstract ClienteFrecuente consultarClienteFrecuentePorTelefono(String telefono) throws PersistenciaException;
+
     public abstract ClienteFrecuente consultarClienteFrecuentePorCorreo(String correoElectronico) throws PersistenciaException;
+
+    public abstract List<ClienteFrecuente> consultarTodosClientesFrecuentes() throws PersistenciaException;
 }
