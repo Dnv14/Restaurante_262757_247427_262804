c80e3d459f5a222ca31a7e3aed94740940fca3d5
Dnv14 <diego.navarro.140706@gmail.com>
2026-04-09 02:08:57 -0700

    validaciones BO
    bo

--- RestauranteNegocio_262757_247427_262804/src/main/java/com/mycompany/restaurantenegocio_262757_247427_262804/ProductosBO.java
+++ RestauranteNegocio_262757_247427_262804/src/main/java/com/mycompany/restaurantenegocio_262757_247427_262804/ProductosBO.java
@@ -10,6 +10,8 @@ import com.mycompany.restaurantedtos_262757_247427_262804.NuevoProductoDTO;
 import com.mycompany.restaurantepersistencia.IProductoDAO;
 import com.mycompany.restaurantepersistencia.PersistenciaException;
 import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 /**
  *
@@ -23,9 +25,41 @@ public class ProductosBO implements IProductosBO {
         this.productosDAO = productosDAO;
     }
 
+    private boolean soloLetras(String texto) {
+        Pattern pattern = Pattern.compile("^[a-zA-ZáéíóúÁÉÍÓÚñÑ ]+$");
+        Matcher matcher = pattern.matcher(texto);
+        return matcher.matches();
+    }
+
     @Override
     public Producto validarRegistroProducto(NuevoProductoDTO productoDTO) throws NegocioException {
-        //TODO validaciones
+        if (productoDTO == null) {
+            throw new NegocioException("La información del producto está vacía.");
+        }
+
+        if (productoDTO.getNombre() == null || productoDTO.getNombre().trim().isEmpty()) {
+            throw new NegocioException("El nombre del producto es obligatorio.");
+        }
+
+        if (!soloLetras(productoDTO.getNombre())) {
+            throw new NegocioException("El nombre solo debe contener letras.");
+        }
+
+        if (productoDTO.getDescripcion().length() > 50) {
+            throw new NegocioException("La descripción es muy larga (máximo 50 caracteres)");
+        }
+        
+        if (productoDTO.getPrecio() == null) {
+            throw new NegocioException("El precio es obligatorio.");
+        }
+        
+        if (productoDTO.getPrecio() < 0) {
+            throw new NegocioException("El precio debe ser un número positivo.");
+        }
+
+        if (productoDTO.getRecetas() == null || productoDTO.getRecetas().isEmpty()) {
+            throw new NegocioException("Un producto debe tener al menos un ingrediente.");
+        }
 
         try {
             Producto producto = productosDAO.agregarProducto(productoDTO);
@@ -41,7 +75,12 @@ public class ProductosBO implements IProductosBO {
 
     @Override
     public List<Producto> validarBarraBusquedaProductos(String nombre) throws NegocioException {
-        //TODO validaciones
+        if (nombre != null && !nombre.isEmpty()) {
+            if (!soloLetras(nombre)) {
+                throw new NegocioException("La búsqueda solo debe contener letras");
+            }
+
+        }
         try {
             if (nombre == null || nombre.isEmpty()) {
                 return productosDAO.consultarTodosLosProductos();
@@ -71,6 +110,14 @@ public class ProductosBO implements IProductosBO {
         if (productoActualizar.getId() == null) {
             throw new NegocioException("El ID del producto es nulo");
         }
+
+        if (!soloLetras((productoActualizar.getNombre()))) {
+            throw new NegocioException("El nombre actualizado no es válido.");
+        }
+
+        if (productoActualizar.getPrecio() < 0) {
+            throw new NegocioException("El precio debe ser un número positivo.");
+        }
         try {
             return productosDAO.actualizarProducto(productoActualizar);
         } catch (PersistenciaException ex) {
@@ -95,7 +142,11 @@ public class ProductosBO implements IProductosBO {
         if (id == null) {
             throw new NegocioException("El ID del producto es nulo");
         }
-        
+
+        if (estadoDTO == null) {
+            throw new NegocioException("El nuevo estado es obligatorio.");
+        }
+
         try {
             productosDAO.actualizarEstadoProducto(id, estadoDTO);
         } catch (PersistenciaException ex) {
