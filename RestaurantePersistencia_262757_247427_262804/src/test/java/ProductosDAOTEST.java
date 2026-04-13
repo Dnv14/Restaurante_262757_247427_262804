118182186022d2b021573f88c9ce4b70b5b87092
Dnv14 <diego.navarro.140706@gmail.com>
2026-04-13 16:41:26 -0700

    dao
    da

--- RestaurantePersistencia_262757_247427_262804/src/test/java/ProductosDAOTEST.java
+++ RestaurantePersistencia_262757_247427_262804/src/test/java/ProductosDAOTEST.java
@@ -5,9 +5,11 @@
 
 import com.mycompany.restaurantedominio_262757_247427_262804.Producto;
 import com.mycompany.restaurantedtos_262757_247427_262804.EstadoDTO;
+import com.mycompany.restaurantedtos_262757_247427_262804.NuevaRecetaDTO;
 import com.mycompany.restaurantedtos_262757_247427_262804.NuevoProductoDTO;
 import com.mycompany.restaurantedtos_262757_247427_262804.TipoProductoDTO;
 import com.mycompany.restaurantepersistencia.ProductosDAO;
+import java.util.LinkedList;
 import java.util.List;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
@@ -26,25 +28,38 @@ public class ProductosDAOTEST {
         this.dao = new ProductosDAO();
     }
 
-//    @Test
-//    public void crearProductoSiLoCrea() {
-//        NuevoProductoDTO productoDTO = new NuevoProductoDTO("Papas fritas ", "Papas freidas", 150.00, TipoProductoDTO.PLATILLO, EstadoDTO.ACTIVO);
-//        
-//        assertDoesNotThrow(() -> {
-//            Producto producto = dao.agregarProducto(productoDTO);
-//            producto.getId();
-//        });
-//    }
-//    
-//    @Test
-//    public void conmsultarProductosNombreFunciona() {
-//        assertDoesNotThrow(() -> {
-//            List<Producto> productos = dao.consultarProductosNombre("papas");
-//            
-//            for(Producto productosNombre : productos){
-//                System.out.println(productosNombre.getNombre());
-//            }
-//            System.out.println(productos.size());
-//        });
-//    }
+    @Test
+    public void crearProductoSiLoCrea() {
+        NuevoProductoDTO productoDTO = new NuevoProductoDTO("Papas fritas ", "Papas freidas", 150.00, TipoProductoDTO.PLATILLO, EstadoDTO.ACTIVO);
+        productoDTO.setRecetas(new LinkedList<>());
+
+        assertDoesNotThrow(() -> {
+            Producto producto = dao.agregarProducto(productoDTO);
+            producto.getId();
+        });
+    }
+
+    @Test
+    public void conmsultarProductosNombreFunciona() {
+        assertDoesNotThrow(() -> {
+            List<Producto> productos = dao.consultarProductosNombre("papas");
+
+            for (Producto productosNombre : productos) {
+                System.out.println(productosNombre.getNombre());
+            }
+            System.out.println(productos.size());
+        });
+    }
+
+    @Test
+    public void eliminarProductoFunciona() {
+        assertDoesNotThrow(() -> {
+
+            dao.eliminarProducto(1L);
+
+            Producto consultado = dao.consultarProductoPorId(1L);
+
+            assertNull(consultado, "El producto con ID 11 aún existe en la base de datos.");
+        });
+    }
 }
